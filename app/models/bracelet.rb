7bd1e02ee1965f3debea9a3b71e2acb7c73b555f
Katarzyna Kasprzak <kasia.kasprzak18@gmail.com>
2015-04-22 14:25:13 +0200

    tablecustom

--- app/models/bracelet.rb
+++ app/models/bracelet.rb
@@ -4,11 +4,25 @@ class Bracelet < ActiveRecord::Base
   after_create :create_color_positions
   
   def create_color_positions
-    (1..30).each do |x|
+    (1..(25*8)).each do |x|
       colorPosition = ColorPosition.create(position: x, color_id: 1)
       colorPosition.bracelet = self
       colorPosition.save!
     end 
+  end
+  
+  def positions_in_first_look_for_iteration iteration
+    row_width = 8
+    number_of_columns = (iteration % 2) == 0 ? row_width : row_width + 1
+
+    idx = iteration * row_width + (iteration)
+    
+    return self.color_positions.slice(idx, number_of_columns)
+  
+  end
+  
+  def rows_count
+    return 25
   end
   
 end
